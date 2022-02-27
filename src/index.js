f4de6cd4f1c61323c5f1f7e9fa1fd3b1b9e8c1b5
PaoMo <paomo5515@qq.com>
2022-02-27 20:02:10 +0800

    learn react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
+
+import axios from 'axios';
 import App from './App';
 
+// 默认配置
+axios.defaults.baseURL = "https://httpbin.org" 
+axios.defaults.headers.common["token"] = "adada"
 
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('root')
 );
 
